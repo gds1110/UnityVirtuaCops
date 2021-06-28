4730d8d4b95f851d892216b396067312f2e025a5
baedole1 <qodydwls1013@naver.com>
2021-06-28 21:48:11 +0900

    error fix
    sibal

--- Assets/Script/Script_Bae/Grenade.cs
+++ Assets/Script/Script_Bae/Grenade.cs
@@ -28,6 +28,15 @@ public class Grenade : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player") && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
     void Explode()
     {
         Instantiate(exlposionEffect, transform.position, transform.rotation);
@@ -35,11 +44,11 @@ public class Grenade : MonoBehaviour
         Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
         foreach (Collider nearbyObject in collidersToDestroy)
         {
-            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-            if(rb != null)
-            {
-                rb.AddExplosionForce(force, transform.position, radius);
-            }
+            //Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            //if(rb != null)
+            //{
+            //    rb.AddExplosionForce(force, transform.position, radius);
+            //}
 
             Destructible dest = nearbyObject.GetComponent<Destructible>();
             if (dest != null)
@@ -49,14 +58,14 @@ public class Grenade : MonoBehaviour
         }
 
         Collider[] collidersToMove = Physics.OverlapSphere(transform.position, radius);
-        foreach (Collider nearbyObject in collidersToMove)
-        {
-            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.AddExplosionForce(force, transform.position, radius);
-            }
-        }
+        //foreach (Collider nearbyObject in collidersToMove)
+        //{
+        //    Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+        //    if (rb != null)
+        //    {
+        //        rb.AddExplosionForce(force, transform.position, radius);
+        //    }
+        //}
 
         // ªË¡¶
         Destroy(gameObject);
