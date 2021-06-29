cf84e4b6a6e51d4a60c1e03ce1e3c94c6f2e695e
kuksally <wow_1115@naver.com>
2021-06-29 12:42:09 +0900

    UI 수정, 버튼 색 변경 이벤트 추가

--- Assets/Script/Sally/ColorChange.cs
+++ Assets/Script/Sally/ColorChange.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class ColorChange : MonoBehaviour
+public class ColorChange : MonoBehaviour, /*ISelectHandler,*/ IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
+    Text text;
+
     void Start()
     {
-        
+        text = transform.GetComponentInChildren<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // When highlighted with mouse.
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        text.color = Color.yellow;
+
+        // Do something.
+        //Debug.Log("<color=red>Event:</color> Completed mouse highlight.");
     }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = Color.white;
+    }
+    // When selected.
+    //public void OnSelect(BaseEventData eventData)
+    //{
+    //    // Do something.
+    //    Debug.Log("<color=red>Event:</color> Completed selection.");
+    //}
 }
